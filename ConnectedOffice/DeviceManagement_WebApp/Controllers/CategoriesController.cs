5ba10b1c22f4ad2b31fcc0de79d2a13d3a093e16
31993729 <francoiss616@gmail.com>
2022-09-29 13:31:08 +0200

    Final version of project 3

--- ConnectedOffice/DeviceManagement_WebApp/Controllers/CategoriesController.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Controllers/CategoriesController.cs
@@ -79,8 +79,7 @@ namespace DeviceManagement_WebApp.Controllers
         }
 
         // POST: Categories/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(Guid id, [Bind("CategoryId,CategoryName,CategoryDescription,DateCreated")] Category category)
